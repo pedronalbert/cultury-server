3ffccac3e2643ea6b9890d1e2bbb62e63293298c
Alberto Pedron <pedron.albert@gmail.com>
2016-05-22 00:32:20 -0400

    Add normal user login test in PublishRequestController

--- test/unit/PublishRequestsController.test.js
+++ test/unit/PublishRequestsController.test.js
@@ -1,8 +1,18 @@
 'use strict';
 let request = require('supertest');
+let faker = require('faker');
+
+let generateNewData = () => {
+  return {
+    title: faker.lorem.text(),
+    content: faker.lorem.text(),
+    imageUrl: faker.image.imageUrl(),
+    category: faker.lorem.word()
+  }
+};
 
 describe('PublishRequestsController', () => {
-  describe.only('Without Login', () => {
+  describe('Without Login', () => {
     describe('#index', () => {
       it('Response 401', done => {
         request(sails.hooks.http.app)
@@ -46,6 +56,80 @@ describe('PublishRequestsController', () => {
     describe('#denyAction', () => {
       it('Response 401', done => {
         request(sails.hooks.http.app)
+          .post('/publish-requests/1/actions/deny')
+          .expect(401, done);
+      });
+    });
+  });
+
+  describe('Normal User login', () => {
+    let createData = generateNewData();
+    let publishRequestCreated;
+    let userRequest;
+
+    before(done => {
+      let userData = fixtures['user'][1];
+      userRequest = request.agent(sails.hooks.http.app);
+
+      userRequest
+        .post('/login')
+        .send({
+          email: userData.email,
+          password: userData.password
+        })
+        .expect(200, done);
+    });
+
+    describe('#index', () => {
+      it('Response 200', done => {
+        userRequest
+          .get('/publish-requests')
+          .expect(200, done)
+      });
+    })
+
+    describe('#create', () => {
+      it('Response 201', done => {
+        userRequest
+          .post('/publish-requests')
+          .send(createData)
+          .expect(201)
+          .end((err, res) => {
+            if (err) return done(err);
+            publishRequestCreated = res.body.data;
+            done();
+          });
+      });
+    });
+
+    describe('#show', () => {
+      it('Response 200', done => {
+        userRequest
+          .get('/publish-requests/' + publishRequestCreated.id)
+          .expect(200, done);
+      });
+    });
+
+    describe('#update', () => {
+      it('Response 200', done => {
+        userRequest
+          .put('/publish-requests/1')
+          .send(generateNewData())
+          .expect(200, done);
+      });
+    });
+
+    describe('#publishAction', () => {
+      it('Response 401 por ser usuario', done => {
+        userRequest
+          .post('/publish-requests/1/actions/publish')
+          .expect(401, done);
+      });
+    });    
+
+    describe('#denyAction', () => {
+      it('Response 401 por ser usuario', done => {
+        userRequest
           .post('/publish-requests/1/actions/deny')
           .expect(401, done);
       });

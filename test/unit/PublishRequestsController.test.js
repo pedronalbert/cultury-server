e9166294e8adb78a2be6221f504aaf8e2ad96919
Alberto Pedron <pedron.albert@gmail.com>
2016-05-23 20:14:38 -0400

    Change PublishRequestsController test structure

--- test/unit/PublishRequestsController.test.js
+++ test/unit/PublishRequestsController.test.js
@@ -1,6 +1,7 @@
 'use strict';
 let request = require('supertest');
 let faker = require('faker');
+let async = require('async');
 
 let generateNewData = () => {
   return {
@@ -12,173 +13,82 @@ let generateNewData = () => {
 };
 
 describe('PublishRequestsController', () => {
-  describe('Without Login', () => {
-    describe('#index', () => {
-      it('Response 401', done => {
-        request(sails.hooks.http.app)
-          .get('/publish-requests')
-          .expect(401, done);
-      });
-    })
+  let userAgent, adminAgent;
 
-    describe('#create', () => {
-      it('Response 401', done => {
-        request(sails.hooks.http.app)
-          .post('/publish-requests')
-          .expect(401, done);
-      });
-    });
+  before(done => {
+    let adminFixture = fixtures['user'][0];
+    let userFixture = fixtures['user'][1];
 
-    describe('#show', () => {
-      it('Response 401', done => {
-        request(sails.hooks.http.app)
-          .get('/publish-requests/1')
-          .expect(401, done);
-      });
-    });
+    userAgent = request.agent(sails.hooks.http.app);
+    adminAgent = request.agent(sails.hooks.http.app);
 
-    describe('#update', () => {
-      it('Response 401', done => {
-        request(sails.hooks.http.app)
-          .put('/publish-requests/1')
-          .expect(401, done);
-      });
-    });
-
-    describe('#publishAction', () => {
-      it('Response 401', done => {
-        request(sails.hooks.http.app)
-          .post('/publish-requests/1/actions/publish')
-          .expect(401, done);
-      });
-    });    
-
-    describe('#denyAction', () => {
-      it('Response 401', done => {
-        request(sails.hooks.http.app)
-          .post('/publish-requests/1/actions/deny')
-          .expect(401, done);
-      });
-    });
-  });
-
-  describe('User login', () => {
-    let createData = generateNewData();
-    let publishRequestCreated;
-    let userRequest;
-
-    before(done => {
-      let userData = fixtures['user'][1];
-      userRequest = request.agent(sails.hooks.http.app);
-
-      userRequest
-        .post('/login')
-        .send({
-          email: userData.email,
-          password: userData.password
-        })
-        .expect(200, done);
-    });
-
-    describe('#index', () => {
-      it('Response 200', done => {
-        userRequest
-          .get('/publish-requests')
-          .expect(200, done)
-      });
-    })
-
-    describe('#create', () => {
-      it('Response 201', done => {
-        userRequest
-          .post('/publish-requests')
-          .send(createData)
-          .expect(201)
+    //Login Agents
+    async.parallel([
+      next => {
+        userAgent
+          .post('/login')
+          .send({
+            email: userFixture.email,
+            password: userFixture.password
+          })
+          .expect(200)
           .end((err, res) => {
             if (err) return done(err);
-            publishRequestCreated = res.body.data;
-            done();
+            next(null, res.body);
           });
-      });
-    });
+      },
 
-    describe('#show', () => {
-      it('Response 200', done => {
-        userRequest
-          .get('/publish-requests/' + publishRequestCreated.id)
-          .expect(200, done);
-      });
-    });
-
-    describe('#update', () => {
-      it('Response 200', done => {
-        userRequest
-          .put('/publish-requests/' + publishRequestCreated.id)
-          .send(generateNewData())
-          .expect(200, done);
-      });
-
-      it('Response 401 si el artículo no le pertenece', done => {
-        let otherPublishRequest = fixtures['publishrequest'][0];
-
-        userRequest
-          .put('/publish-requests/' + otherPublishRequest.id)
-          .send(generateNewData())
-          .expect(401, done);
-      })
-    });
-
-    describe('#publishAction', () => {
-      it('Response 401 por ser usuario', done => {
-        userRequest
-          .post('/publish-requests/1/actions/publish')
-          .expect(401, done);
-      });
-    });    
-
-    describe('#denyAction', () => {
-      it('Response 401 por ser usuario', done => {
-        userRequest
-          .post('/publish-requests/1/actions/deny')
-          .expect(401, done);
-      });
+      next => {
+        adminAgent
+          .post('/login')
+          .send({
+            email: adminFixture.email,
+            password: adminFixture.password
+          })
+          .expect(200)
+          .end((err, res) => {
+            if (err) return done(err);
+            next(null, res.body);
+          }); 
+      }
+    ], (err, results) => {
+      done();
     });
   });
 
-  describe('Admin login', () => {
-    let adminRequest;
+  describe('#index', () => {
+    describe('Guest', () => {
+      it('Responder 401', done => {
+        request(sails.hooks.http.app)
+          .get('/publish-requests')
+          .expect(401, done)
+      });
+    })
 
-    before(done => {
-      let adminData = fixtures['user'][0];
-      adminRequest = request.agent(sails.hooks.http.app);
+    describe('User', done => {
+      it('Responder 200', done => {
+        userAgent
+          .get('/publish-requests')
+          .expect(200, done)
+      })
+    });
+  });
 
-      adminRequest
-        .post('/login')
-        .send({
-          email: adminData.email,
-          password: adminData.password
-        })
-        .expect(200, done);
+  describe('#create', () => {
+    describe('Guest', () => {
+      it('Responder 401', done => {
+        request(sails.hooks.http.app)
+          .post('/publish-requests')
+          .expect(401, done);
+      });
     });
 
-    describe('#publishAction', () => {
-      it('Response 201', done => {
-        let publishRequest = fixtures['publishrequest'][1];
-
-        adminRequest
-          .post('/publish-requests/' + publishRequest.id  +'/actions/publish')
+    describe('User', () => {
+      it('Responder 201', done => {
+        userAgent
+          .post('/publish-requests')
           .send(generateNewData())
           .expect(201, done);
-      });
-    });    
-
-    describe('#denyAction', () => {
-      it('Response 200', done => {
-        let publishRequest = fixtures['publishrequest'][2];
-
-        adminRequest
-          .post('/publish-requests/' + publishRequest.id  +'/actions/deny')
-          .expect(200, done);
       });
     });
   });
